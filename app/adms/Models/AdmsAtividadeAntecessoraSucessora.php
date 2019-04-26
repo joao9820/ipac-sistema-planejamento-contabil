f9ef459f11938613c95980712f3c7adb96fc4c37
Dhemes Mota <dhemes@ipaconline.com.br>
2019-04-26 15:15:35 -0300

    New Class AdmsAtividadeAtecessoraSucessora

---
+++ app/adms/Models/AdmsAtividadeAntecessoraSucessora.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dhemes
+ * Date: 15/02/2019
+ * Time: 16:11
+ */
+
+namespace App\adms\Models;
+
+
+if (!defined('URL')) {
+    header("Location: /");
+    exit();
+}
+
+class AdmsAtividadeAntecessoraSucessora
+{
+    
+}
